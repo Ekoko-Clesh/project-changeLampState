95d44e1c9a1260fb75119c1a99590043d344a1b1
Clesh <36325257+Gabriel-1999@users.noreply.github.com>
2021-03-29 20:47:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let buttons = document.getElementById("buttons").addEventListener("click", function(Event) {
+  changeState[Event.target.id]();
+});
+
+//OBJECTO CHANGESTATE
+let changeState = {
+    "ligar": () => imagem.src = "./img/ligada.jpg",
+  
+    "desligar" : () => imagem.src =  "./img/desligada.jpg",
+  
+    "quebrar" : () => imagem.src = "./img/quebrada.jpg",
+}
+
+function overMouse() {
+  imagem.src = changeState["ligar"]();
+}
+function outMouse() {
+  imagem.src = changeState["desligar"]();
+}
+let imagem = document.getElementById("img-content");
+let mousOver =  document.getElementById("img-content").addEventListener("mouseover", overMouse);
+let mouseOut = document.getElementById("img-content").addEventListener("mouseout", outMouse);
